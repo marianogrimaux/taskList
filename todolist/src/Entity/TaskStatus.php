4f32126086e77e0849924b53e96a932bf7890761
marianogrimaux <mariano.grimaux@gmail.com>
2020-09-19 21:15:41 -0300

    Create enum types

--- todolist/src/Entity/TaskStatus.php
+++ todolist/src/Entity/TaskStatus.php
@@ -10,4 +10,19 @@ class TaskStatus extends Enum
     private const PENDING = 'Pending';
     private const DONE = 'Done';
     private const DOING = 'Doing';
+
+    public static function PENDING() : self
+    {
+        return new TaskStatus(self::PENDING);
+    }
+
+    public static function DONE() : self
+    {
+        return new TaskStatus(self::DONE);
+    }
+
+    public static function DOING() : self
+    {
+        return new TaskStatus(self::DOING);
+    }
 }
