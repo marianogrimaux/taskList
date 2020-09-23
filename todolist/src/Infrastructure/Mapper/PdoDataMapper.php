1a2401ef65ef410f722390568e5821b2e27641c9
marianogrimaux <mariano.grimaux@gmail.com>
2020-09-22 21:48:15 -0300

    Refactor: Extract select method to super class. Also  add abstract protected function to get the mapper table.

--- todolist/src/Infrastructure/Mapper/PdoDataMapper.php
+++ todolist/src/Infrastructure/Mapper/PdoDataMapper.php
@@ -4,12 +4,39 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Mapper;
 
+use PDO;
+use PDOException;
+
 abstract class PdoDataMapper
 {
     protected $dbConnection;
 
-    function __construct(\PDO $dbConnection)
+    function __construct(PDO $dbConnection)
     {
         $this->dbConnection = $dbConnection;
     }
+
+    protected function executeSelect(array $valuesMap): ?array
+    {
+        $sql = 'SELECT * FROM ' . $this->getTableName() . ' WHERE ';
+        $columns = [];
+        if (count($valuesMap) > 1) {
+            foreach (array_keys($valuesMap) as $column) {
+                $columns[] = $column . '=?';
+            }
+            $sql .= implode(' AND ', $columns);
+        } else {
+            $sql .= array_keys($valuesMap)[0] . '=?';
+        }
+        try {
+            $statement = $this->dbConnection->prepare($sql);
+            $statement->execute(array_values($valuesMap));
+            $fetchedData = $statement->fetch(PDO::FETCH_ASSOC);
+            return $fetchedData ?: null;
+        } catch (PDOException $exception) {
+            throw new MapperException($exception->getMessage(), (int)$exception->getCode(), $exception);
+        }
+    }
+
+    abstract protected function getTableName(): string;
 }
