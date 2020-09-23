4e4464e9446f18e59b4438efd95397ad9df5fb12
marianogrimaux <mariano.grimaux@gmail.com>
2020-09-23 01:17:38 -0300

    Add new test for task data mapper

---
+++ todolist/tests/Infrastructure/Mapper/PdoTaskMapperTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Infrastructure\Mapper;
+
+use App\Infrastructure\Mapper\Task\PdoMapperTest;
+
+class PdoTaskMapperTest extends PdoMapperTest
+{
+    public function testCreate(): void
+    {
+        $this->fail('Not done');
+    }
+
+    public function testUpdate(): void
+    {
+        $this->fail('Not done');
+    }
+
+    public function testDelete(): void
+    {
+        $this->fail('Not done');
+    }
+
+    public function testFetchOne(): void
+    {
+        $this->fail('Not done');
+    }
+
+    public function testFetchBy(): void
+    {
+        $this->fail('Not done');
+    }
+
+
+}
